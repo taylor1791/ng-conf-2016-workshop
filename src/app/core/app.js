8a00145c91d8c0c67d2b8ac0ee451c6bc85fec59
Taylor Everding <Taylor@Everding.email>
2016-03-01 19:21:56 -0700

    Add webpack directive pattern

--- src/app/core/app.js
+++ src/app/core/app.js
@@ -4,9 +4,10 @@ var DINO_MAIL = 'dino.mail'
 require('normalize.css/normalize.css');
 
 angular.module(DINO_MAIL, [
-    require('angular-ui-router')
-  ])
-  .config(require('./routes.js'));
+    require('angular-ui-router'),
+    require('./routes.js'),
+    require('./directives.js'),
+  ]);
 
 angular.element(document).ready(function() {
   angular.bootstrap(document.getElementById('app'), [DINO_MAIL], {strictDi: true});
