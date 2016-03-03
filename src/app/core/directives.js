8d1c85c0f34ebbc2fe50e5de41a38c79e5208975
Taylor Everding <Taylor@Everding.email>
2016-03-02 21:19:46 -0700

    Add omni-search to header

--- src/app/core/directives.js
+++ src/app/core/directives.js
@@ -2,5 +2,6 @@
 module.exports = function($compileProvider) {
   $compileProvider.directive('dmLayout', require('../layout/directive.js'));
   $compileProvider.directive('dmHeader', require('../header/directive.js'));
+  $compileProvider.directive('dmSearchBox', require('../search-box/directive.js'));
 };
 
