05fb1c0df63b402efac980bcf3f6fbefe9754a12
Taylor Everding <Taylor@Everding.email>
2016-03-02 22:15:36 -0700

    Add account info to header

--- src/app/core/directives.js
+++ src/app/core/directives.js
@@ -3,5 +3,6 @@ module.exports = function($compileProvider) {
   $compileProvider.directive('dmLayout', require('../layout/directive.js'));
   $compileProvider.directive('dmHeader', require('../header/directive.js'));
   $compileProvider.directive('dmSearchBox', require('../search-box/directive.js'));
+  $compileProvider.directive('dmSmallAvatar', require('../small-avatar/directive.js'));
 };
 
