53aab56c51dcd9e1c9691fe80b78cb1113996ff8
Taylor Everding <Taylor@Everding.email>
2016-04-12 19:47:50 -0600

    Mark labe active on route change

--- src/app/label-list/controller.js
+++ src/app/label-list/controller.js
@@ -1,22 +1,28 @@
 // @ngInject
-module.exports = function($stateParams, API) {
+module.exports = function($stateParams, $rootScope, API) {
   var vm = this;
 
   vm.showBottom = false;
   vm.top = [];
   vm.bottom = [];
-  vm.activeLabel = $stateParams.label || 'inbox';
 
   API.getLabels().then(setLabels);
+  setLabel();
 
   vm.showUnread = function(label) {
-    return label.estimatedUnread > 0 && 
+    return label.estimatedUnread > 0 &&
       !~['ALL MAIL', 'TRASH', 'SENT MAIL'].indexOf(label.label.toUpperCase());
   };
 
   vm.toggleBottom = function() {
     vm.showBottom = !vm.showBottom;
   };
+
+  $rootScope.$on('$stateChangeSuccess', setLabel);
+
+  function setLabel() {
+    vm.activeLabel = $stateParams.label || 'inbox';
+  }
 
   function setLabels(labels) {
     vm.top = labels.high;
