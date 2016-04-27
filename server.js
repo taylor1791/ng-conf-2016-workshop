2a8749b6ba2989e1a81e1ad897467829d34cf773
Taylor Everding <Taylor@Everding.email>
2016-04-26 23:06:20 -0600

    Add jitter to ajax responses

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ var express = require('express');
 var simpleFsRest = require('simple-fs-rest');
 var app = express();
 
+var MAX_TIME = 100;
+
 // Link the REST Service to anything under /api. ex: /api/users/
-app.use('/api', simpleFsRest());
+app.use('/api', function(req, res, next) {
+  var server = simpleFsRest();
+
+  setTimeout(function() {
+    server(req, res, next);
+  }, Math.random() * MAX_TIME);
+});
 
 app.listen(8081);
 
