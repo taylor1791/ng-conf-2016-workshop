b6eca23a5fcc5406f12a75740cacf5ee00d8a2d3
Taylor Everding <Taylor@Everding.email>
2016-04-27 20:48:36 -0600

    Make ajax have constant latency

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var simpleFsRest = require('simple-fs-rest');
 var app = express();
 
-var MAX_TIME = 100;
+var TIME_DELAY = 135;
 
 // Link the REST Service to anything under /api. ex: /api/users/
 app.use('/api', function(req, res, next) {
@@ -11,7 +11,7 @@ app.use('/api', function(req, res, next) {
 
   setTimeout(function() {
     server(req, res, next);
-  }, Math.random() * MAX_TIME);
+  }, TIME_DELAY);
 });
 
 app.listen(8081);
