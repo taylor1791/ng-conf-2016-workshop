17c3bb5297b72c05b0b28a0b798c2ec0561ecda2
Taylor Everding <Taylor@Everding.email>
2016-04-30 23:54:16 -0600

    Add markdown email preview

--- src/app/compose/controller.js
+++ src/app/compose/controller.js
@@ -1,5 +1,21 @@
+var marked = require('marked');
+
+var markedOptions = {sanitize: true};
+
 // @ngInject
-module.exports = function ComposeCtrl($rootScope, $scope) {
+module.exports = function ComposeCtrl($element, $rootScope, $compile, User) {
   var vm = this;
+  var previewPane = $element.find('preview-mail-column');
+  var previewScope = $rootScope.$new();
+
+  vm.body = "Hello ,\n\n\\- {{user.displayName}} -\n\n---\n{{user.signature}}";
+
+  vm.updatePreview = function() {
+    previewScope.user = User;
+    previewPane.empty();
+    previewPane.append($compile(marked(vm.body, markedOptions))(previewScope));
+  };
+
+  vm.updatePreview();
 };
 
