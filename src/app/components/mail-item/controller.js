55ac47db6184c22381597f39cf638747c1cacd66
Taylor Everding <Taylor@Everding.email>
2016-05-02 16:11:10 -0600

    Make select all button work

--- src/app/components/mail-item/controller.js
+++ src/app/components/mail-item/controller.js
@@ -1,5 +1,13 @@
 // @ngInject
-module.exports = function() {
+module.exports = function($scope, $timeout) {
   var vm = this;
+
+  // I know that there are better ways to do this, but I need to show this pattern
+  $scope.$on('SelectEmail', function() {
+    //Pretend there is an animation there or something
+    $timeout(function() {
+      vm.mail.selected = true;
+    });
+  });
 };
 
--- src/app/components/mailbox-header/controller.js
+++ src/app/components/mailbox-header/controller.js
@@ -1,8 +1,12 @@
 // @ngInject
-module.exports = function MailboxHeaderCtrl(EmailState) {
+module.exports = function MailboxHeaderCtrl($rootScope, EmailState) {
   var vm = this;
 
   vm.selectedEmail = function() {
     return EmailState.emailSelected();
-  }
+  };
+
+  vm.selectAll = function() {
+    $rootScope.$broadcast('SelectEmail');
+  };
 };
